d16aac81c96c1ac8d6d8463b2752adab526f33d9
root <root@ns338374.ip-37-187-166.eu>
2016-11-08 05:31:13 +0100

    Fixed excluding TheCenter mod

--- Ark_Mod_Downloader.py
+++ Ark_Mod_Downloader.py
@@ -141,12 +141,12 @@ class ArkModDownloader():
         Build a list of all installed mods by grabbing all directory names from the mod folder
         :return:
         """
-        exclude = set(TheCenter)
+        exclude = set(["TheCenter"])
         if not os.path.isdir(os.path.join(self.working_dir, "ShooterGame/Content/Mods")):
             return
         for curdir, dirs, files in os.walk(os.path.join(self.working_dir, "ShooterGame/Content/Mods")):
-            dirs[:] = [d for d in dirs if d not in exclude]
             for d in dirs:
+                dirs[:] = [d for d in dirs if d not in exclude]
                 self.installed_mods.append(d)
             break
 
